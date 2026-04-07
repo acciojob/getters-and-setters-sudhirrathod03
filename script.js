9afcae953f9ac71af7a076aa8283abd0a28bfc51
Sudhir Rathod <125038605+sudhirrathod03@users.noreply.github.com>
2026-04-07 19:48:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,18 @@ class Person {
 		this._age = age;
 	}
 
+	// getter for name
+	get name() {
+		return this._name;
+	}
+
 	getName(){
 		return this._name;
+	}
+
+	// getter + setter for age
+	get age() {
+		return this._age;
 	}
 	
 	set age(value) {
@@ -15,19 +25,19 @@ class Person {
 
 class Student extends Person {
 	study(){
-		console.log(this._name + " is studying");
+		console.log(this.name + " is studying");
 	}	
 }
 
 class Teacher extends Person {
 	teach(){
-		console.log(this._name + " is teaching");
+		console.log(this.name + " is teaching");
 	}	
 }
 
 const person = new Person("John", 25);
 
-// Do not change the code below this line
+// Do not change below
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
